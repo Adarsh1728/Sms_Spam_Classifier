3ab29581f4c362c476ba5e2b5b13805cf215984e
Adarsh1728 <adarshdubey1728>
2025-05-09 02:16:38 +0530

    first commit

--- rout.ts
+++ rout.ts
@@ -1,13 +1,12 @@
 fetch('http://127.0.0.1:5000/predict', {
-    method: 'POST', // Must be POST
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      message: "Congratulations! You've won a free iPhone."
-    }),
-  })
-    .then(res => res.text())
-    .then(data => console.log(data))
-    .catch(err => console.error("Error:", err));
-  
+  method: 'POST', // Must be POST
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: new URLSearchParams({
+    message: "Congratulations! You've won a free iPhone."
+  }),
+})
+  .then(res => res.text())
+  .then(data => console.log(data))
+  .catch(err => console.error("Error:", err));
